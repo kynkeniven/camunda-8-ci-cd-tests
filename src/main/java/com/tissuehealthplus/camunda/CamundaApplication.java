40499a107c7c6494646e721588033f2a13897d02
Kynkeniven Grosskopf <kynkeniven.grosskopf@ntconsultcorp.com>
2025-02-06 15:18:30 -0300

    update project

--- src/main/java/com/tissuehealthplus/camunda/CamundaApplication.java
+++ src/main/java/com/tissuehealthplus/camunda/CamundaApplication.java
@@ -1,9 +1,12 @@
 package com.tissuehealthplus.camunda;
 
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@Deployment
+@Deployment(resources = "classpath*:/processes/**/*.bpmn")
 public class CamundaApplication {
 
 	public static void main(String[] args) {
