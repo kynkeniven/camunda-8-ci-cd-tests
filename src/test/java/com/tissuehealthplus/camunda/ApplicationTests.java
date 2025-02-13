70348be6781b736c645f4f84882060f0a8719ae1
Kynkeniven Grosskopf <kynkeniven.grosskopf@ntconsultcorp.com>
2025-02-13 14:11:30 -0300

    update project

--- src/test/java/com/tissuehealthplus/camunda/ApplicationTests.java
+++ src/test/java/com/tissuehealthplus/camunda/ApplicationTests.java
@@ -3,8 +3,8 @@ package com.tissuehealthplus.camunda;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CamundaApplicationTests {
+
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
